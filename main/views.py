1fcd0f666986f3afb93b0d7c04dc9710832f565a
Iskander <iskandarbeksaydullaev2006@gmail.com>
2025-03-27 21:49:15 +0500

    Первый коммит

--- main/views.py
+++ main/views.py
@@ -3,20 +3,6 @@ from django.shortcuts import render
 from .forms import UploadFileForm
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-
-# def process_file(file):
-#     text = file.read().decode("utf-8")
-#     words = text.split()  
-#     vectorizer = TfidfVectorizer()
-#     tfidf_matrix = vectorizer.fit_transform([" ".join(words)])
-#     tfidf_scores = dict(zip(vectorizer.get_feature_names_out(), tfidf_matrix.toarray()[0]))
-    
-#     sorted_tfidf = sorted(tfidf_scores.items(), key=lambda x: x[1], reverse=True)[:50]
-#     return sorted_tfidf
-
-
-
-# def upload_file(request):
 #     if request.method == "POST":
 #         form = UploadFileForm(request.POST, request.FILES)
 #         if form.is_valid():
@@ -30,13 +16,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import chardet
 
 def process_file(file_field):
-    # Определяем кодировку файла
     with open(file_field.path, "rb") as f:
         raw_data = f.read()
         result = chardet.detect(raw_data)
         encoding = result["encoding"] if result["encoding"] else "utf-8"
 
-    # Открываем файл с найденной кодировкой
     with open(file_field.path, "r", encoding=encoding, errors="ignore") as f:
         text = f.read()
 
